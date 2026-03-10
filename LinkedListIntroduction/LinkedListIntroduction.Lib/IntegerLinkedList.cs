e4c87ea97fcb83afe08c99d6d7bfde631dfb65bd
phamd2 <phamd@stpaulsschool.org.uk>
2026-03-10 09:26:08 +0000

    c

--- LinkedListIntroduction/LinkedListIntroduction.Lib/IntegerLinkedList.cs
+++ LinkedListIntroduction/LinkedListIntroduction.Lib/IntegerLinkedList.cs
@@ -37,23 +37,27 @@ public class IntegerLinkedList
 
 
     }
-    public void Prepend(int v)
+    public void Prepend(int value)
     {
-        IntegerNode? newNode = new IntegerNode(v);
-        newNode.UpdatePointer(_head);
-        _head = newNode;
+        IntegerNode node = new IntegerNode(value);
+
+        if (_head != null)
+        {
+            node.Next = _head;
+        }
+        _head = node;
     }
 
 
-    public bool Delete(int doomed)
+    public bool Delete(int value)
     {
         if (_head != null)
         {
-            if(_head.Value == doomed){
+            if(_head.Value == value){
                 _head = _head.Next;
                 return true;
             }
-            return _head.Delete(doomed);
+            return _head.Delete(value);
         } return false;
     }
 
@@ -73,212 +77,5 @@ public class IntegerLinkedList
             return true;
         }
     }
-
-
-    public void Join(IntegerLinkedList? addition)
-    {
-        if (addition == null)
-            return;
-        else
-        {
-            if (_head == null)
-                _head = addition._head;
-            else
-                _head.Join(addition._head);
-        }
-    }
-
-
-    public bool Contains(int value)
-    {
-        if(_head == null)
-            return false;
-        return _head.Contains(value);
-    }
-
-
-    public void RemoveDuplicates()
-    {
-        _head?.RemoveDuplicates();
-    }
-
-
-    public void MergeAlternate(IntegerLinkedList addition)
-    {
-        if(_head == null || addition == null || addition._head == null)
-            return;
-        _head.MergeAlternate(addition._head);
-    }
-
-
-    public IntegerLinkedList Reverse()
-    {
-        IntegerLinkedList result = new IntegerLinkedList();
-        _head?.Reverse(result);
-        return result;
-    }
-
-
-    public override string ToString()
-    {
-        return _head == null ? "{}" : $"{{{_head}}}";
-    }
-}
-
-
-public class IntegerNode
-{
-    int _value;
-    IntegerNode _next;
-
-
-    internal int Value => _value;
-    internal IntegerNode? Next => _next;
-    internal int Count => _next == null ? 1 : 1 + _next.Count;
-           
-    internal int Sum => _next == null ? _value : _value + _next.Sum;
-
-
-
-
-    internal IntegerNode(int v)
-    {
-        _value = v;
-        _next = null;
-    }
-    internal IntegerNode(int v, IntegerNode? ptr)
-    {
-        _value = v;
-        _next = ptr;
-    }
-
-
-    internal void UpdatePointer(IntegerNode? ptr)
-    {
-        _next = ptr;
-    }
-
-
-    internal void Append(int v)
-    {
-        if (_next == null)
-            _next = new IntegerNode(v);
-        else
-            _next.Append(v);
-    }
-
-
-    internal bool Delete(int doomed)
-    {
-        if (_next == null) return false;
-        if(_next._value == doomed){
-            _next = _next.Next;
-            return true;
-        }
-        else
-        {
-            return _next.Delete(doomed);
-        }
-    }
-
-
-    internal bool Insert(int value, int position)
-    {
-        if(position == 0)
-        {
-            _next = new IntegerNode(value,_next);
-            return true;
-        }
-        if (_next == null)
-            return false;
-        else
-        {
-            return _next.Insert(value, position - 1);
-        }
-    }
-
-
-    internal void Join(IntegerNode addition)
-    {
-        if(_next == null)
-            _next = addition;
-        else  
-            _next.Join(addition);
-    }
-
-
-    internal bool Contains(int value)
-    {
-        if(_value == value)
-            return true;
-        if(_next == null)
-            return false;
-        return _next.Contains(value);
-    }
-
-
-    internal void RemoveDuplicates()
-    {
-        if(_next==null)
-            return;
-        if (_next.Value == _value)
-        {
-            _next = _next.Next;
-            RemoveDuplicates();
-        }
-        else
-        {
-            _next.RemoveDuplicates();
-        }
-    }
-
-
-    internal void MergeAlternate(IntegerNode addition)
-    {
-        if(addition == null)
-            return;
-
-
-        IntegerNode? next1 = _next;
-        IntegerNode? next2 = addition.Next;
-
-
-        _next = addition;
-        addition.UpdatePointer(next1);
-
-
-        if (next1 != null && next2 != null)
-            next1.MergeAlternate(next2);
-    }
-
-
-    internal void Reverse(IntegerLinkedList list)
-    {
-        list.Prepend(_value);
-
-
-        if (_next != null)
-            _next.Reverse(list);
-    }
-
-
-    internal void InsertSorted(int value)
-    {
-        if(_next == null || value < _next.Value)
-        {
-            _next = new IntegerNode(value, _next);
-        }
-        else
-        {
-            _next.InsertSorted(value);
-        }
-    }
-
-
-    public override string ToString()
-    {
-        return _next == null ? _value.ToString() : $"{_value}, {_next}";
-    }
-
 
 }
