0582ab0c6a39ff75a84d50e1e9522123cbba3df7
phamd2 <phamd@stpaulsschool.org.uk>
2026-03-10 09:02:40 +0000

    code

--- LinkedListIntroduction/LinkedListIntroduction.Lib/IntegerLinkedList.cs
+++ LinkedListIntroduction/LinkedListIntroduction.Lib/IntegerLinkedList.cs
@@ -1,44 +1,144 @@
 ﻿namespace LinkedListIntroduction.Lib;
 
+
 public class IntegerLinkedList
 {
-    IntegerNode _head;
+    public IntegerNode _head;
    
     public IntegerLinkedList()
     {
         _head = null;
     }
 
+
     public IntegerLinkedList(int v)
     {
         _head = new IntegerNode(v);
     }
 
+
+    public IntegerLinkedList(IList<int> ls) : this()
+    {
+        foreach(int i in ls)
+            Append(i);          
+    }
+
+
     public int Count => _head == null ? 0 : _head.Count;
     public int Sum => _head == null ? 0 : _head.Sum;
 
-    public void Append(int v)
+
+    public virtual void Append(int v)
     {
         if (_head == null)
             _head = new IntegerNode(v);
         else
             _head.Append(v);
 
+
     }
+    public void Prepend(int v)
+    {
+        IntegerNode? newNode = new IntegerNode(v);
+        newNode.UpdatePointer(_head);
+        _head = newNode;
+    }
+
+
+    public bool Delete(int doomed)
+    {
+        if (_head != null)
+        {
+            if(_head.Value == doomed){
+                _head = _head.Next;
+                return true;
+            }
+            return _head.Delete(doomed);
+        } return false;
+    }
+
+
+    public bool Insert(int value, int position)
+    {
+        if(position == 0)
+        {
+            Prepend(value);
+            return true;
+        }
+        if (_head == null || _head.Next == null)
+            return false;
+        else
+        {
+            _head.Next.Insert(value, position - 1);
+            return true;
+        }
+    }
+
+
+    public void Join(IntegerLinkedList? addition)
+    {
+        if (addition == null)
+            return;
+        else
+        {
+            if (_head == null)
+                _head = addition._head;
+            else
+                _head.Join(addition._head);
+        }
+    }
+
+
+    public bool Contains(int value)
+    {
+        if(_head == null)
+            return false;
+        return _head.Contains(value);
+    }
+
+
+    public void RemoveDuplicates()
+    {
+        _head?.RemoveDuplicates();
+    }
+
+
+    public void MergeAlternate(IntegerLinkedList addition)
+    {
+        if(_head == null || addition == null || addition._head == null)
+            return;
+        _head.MergeAlternate(addition._head);
+    }
+
+
+    public IntegerLinkedList Reverse()
+    {
+        IntegerLinkedList result = new IntegerLinkedList();
+        _head?.Reverse(result);
+        return result;
+    }
+
 
     public override string ToString()
     {
         return _head == null ? "{}" : $"{{{_head}}}";
     }
 }
+
+
 public class IntegerNode
 {
     int _value;
     IntegerNode _next;
 
-     internal int Count => _next == null ? 1 : 1 + _next.Count;
-            
+
+    internal int Value => _value;
+    internal IntegerNode? Next => _next;
+    internal int Count => _next == null ? 1 : 1 + _next.Count;
+           
     internal int Sum => _next == null ? _value : _value + _next.Sum;
+
+
 
 
     internal IntegerNode(int v)
@@ -46,6 +146,18 @@ public class IntegerNode
         _value = v;
         _next = null;
     }
+    internal IntegerNode(int v, IntegerNode? ptr)
+    {
+        _value = v;
+        _next = ptr;
+    }
+
+
+    internal void UpdatePointer(IntegerNode? ptr)
+    {
+        _next = ptr;
+    }
+
 
     internal void Append(int v)
     {
@@ -55,9 +167,118 @@ public class IntegerNode
             _next.Append(v);
     }
 
+
+    internal bool Delete(int doomed)
+    {
+        if (_next == null) return false;
+        if(_next._value == doomed){
+            _next = _next.Next;
+            return true;
+        }
+        else
+        {
+            return _next.Delete(doomed);
+        }
+    }
+
+
+    internal bool Insert(int value, int position)
+    {
+        if(position == 0)
+        {
+            _next = new IntegerNode(value,_next);
+            return true;
+        }
+        if (_next == null)
+            return false;
+        else
+        {
+            return _next.Insert(value, position - 1);
+        }
+    }
+
+
+    internal void Join(IntegerNode addition)
+    {
+        if(_next == null)
+            _next = addition;
+        else  
+            _next.Join(addition);
+    }
+
+
+    internal bool Contains(int value)
+    {
+        if(_value == value)
+            return true;
+        if(_next == null)
+            return false;
+        return _next.Contains(value);
+    }
+
+
+    internal void RemoveDuplicates()
+    {
+        if(_next==null)
+            return;
+        if (_next.Value == _value)
+        {
+            _next = _next.Next;
+            RemoveDuplicates();
+        }
+        else
+        {
+            _next.RemoveDuplicates();
+        }
+    }
+
+
+    internal void MergeAlternate(IntegerNode addition)
+    {
+        if(addition == null)
+            return;
+
+
+        IntegerNode? next1 = _next;
+        IntegerNode? next2 = addition.Next;
+
+
+        _next = addition;
+        addition.UpdatePointer(next1);
+
+
+        if (next1 != null && next2 != null)
+            next1.MergeAlternate(next2);
+    }
+
+
+    internal void Reverse(IntegerLinkedList list)
+    {
+        list.Prepend(_value);
+
+
+        if (_next != null)
+            _next.Reverse(list);
+    }
+
+
+    internal void InsertSorted(int value)
+    {
+        if(_next == null || value < _next.Value)
+        {
+            _next = new IntegerNode(value, _next);
+        }
+        else
+        {
+            _next.InsertSorted(value);
+        }
+    }
+
+
     public override string ToString()
     {
         return _next == null ? _value.ToString() : $"{_value}, {_next}";
     }
+
 
 }
--- LinkedListIntroduction/LinkedListIntroduction.Tests/BasicLinkedListTests.cs
+++ LinkedListIntroduction/LinkedListIntroduction.Tests/BasicLinkedListTests.cs
@@ -1,10 +1,13 @@
-﻿using LinkedListIntroduction.Lib; 
+﻿using LinkedListIntroduction.Lib;
+
 
 namespace LinkedListIntroduction.Tests;
+
 
 [TestClass]
 public sealed class BasicLinkedListTests
 {
+
 
     [TestMethod]
     public void TestEmpty()
@@ -12,6 +15,7 @@ public sealed class BasicLinkedListTests
         IntegerLinkedList ill = new IntegerLinkedList();
         Assert.AreEqual(0, ill.Count);
     }
+
 
     [TestMethod]
     public void TestCount()
@@ -22,6 +26,7 @@ public sealed class BasicLinkedListTests
         Assert.AreEqual(3, ill.Count);
     }
 
+
     [TestMethod]
     public void TestSum()
     {
@@ -30,6 +35,7 @@ public sealed class BasicLinkedListTests
         ill.Append(9);
         Assert.AreEqual(21, ill.Sum);
     }
+
 
     [TestMethod]
     public void TestToStringExplicit()
@@ -41,4 +47,125 @@ public sealed class BasicLinkedListTests
     }
 
 
+    [TestMethod]
+    public void TestPrepend()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+        ill.Prepend(3);
+        Assert.AreEqual("{3, 5, 7, 9}", ill.ToString());
+    }
+
+
+    [TestMethod]
+    public void TestDeleteWhenPresent()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+        Assert.IsTrue(ill.Delete(7));
+        Assert.AreEqual("{5, 9}", ill.ToString());
+    }
+
+
+    [TestMethod]
+    public void TestDeleteHead()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+        Assert.IsTrue(ill.Delete(5));
+        Assert.AreEqual("{7, 9}", ill.ToString());
+    }
+
+
+    [TestMethod]
+    public void TestDeleteWhenNotPresent()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+        Assert.IsFalse(ill.Delete(6));
+        Assert.AreEqual("{5, 7, 9}", ill.ToString());
+    }
+
+
+    [TestMethod]
+    public void TestInserts()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+        Assert.IsTrue(ill.Insert(8, 0));
+        Assert.AreEqual("{8, 5, 7, 9}", ill.ToString());
+        Assert.IsTrue(ill.Insert(8, 3));
+        Assert.AreEqual("{8, 5, 7, 8, 9}", ill.ToString());
+        Assert.IsFalse(ill.Insert(8, 94));
+    }
+
+
+    [TestMethod]
+    public void TestListInitialisation()
+    {
+        var ill = new IntegerLinkedList([5,7,9]);
+        Assert.AreEqual("{5, 7, 9}", ill.ToString());
+    }
+
+
+    [TestMethod]
+    public void TestJoin()
+    {
+        var ill = new IntegerLinkedList([5,7,9]);
+        var ill2 = new IntegerLinkedList([11,13,17]);
+        ill.Join(ill2);
+        Assert.AreEqual("{5, 7, 9, 11, 13, 17}", ill.ToString());
+    }
+
+
+    [TestMethod]
+    public void TestReverse()
+    {
+        var ill = new IntegerLinkedList([5,7,9]);
+        var ill2 = ill.Reverse();
+        Assert.AreEqual("{9, 7, 5}", ill2.ToString());
+    }
+   
+    [TestMethod]
+    public void TestContainsWhenPresent()
+    {
+        var ill = new IntegerLinkedList([5,7,9]);
+        Assert.IsTrue(ill.Contains(7));
+    }
+
+
+    [TestMethod]
+    public void TestContainsWhenNotPresent()
+    {
+        var ill = new IntegerLinkedList([5,7,9]);
+        Assert.IsFalse(ill.Contains(10));
+    }
+
+
+    [TestMethod]
+    public void TestRemoveDuplicates()
+    {
+        var ill = new IntegerLinkedList([5,7,7,9,9,9]);
+        ill.RemoveDuplicates();
+        Assert.AreEqual("{5, 7, 9}", ill.ToString());
+    }
+
+
+    [TestMethod]
+    public void TestMergeAlternate()
+    {
+        var ill1 = new IntegerLinkedList([1,3,5]);
+        var ill2 = new IntegerLinkedList([2,4,6]);
+
+
+        ill1.MergeAlternate(ill2);
+
+
+        Assert.AreEqual("{1, 2, 3, 4, 5, 6}", ill1.ToString());
+    }
 }
